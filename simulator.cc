03de1c09d92fb983a65a4d7d84bc2a9efd894585
Noah Goldman <noahg34@gmail.com>
2012-06-26 17:13:12 -0400

    removed failed simulator files

--- simulator.cc
+++
@@ -1,7 +0,0 @@
-Simulator::Simulator(int horizontal, int vertical, int robot_size) {
-    x = horizontal;
-    y = vertical;
-    size = robot_size;
-}
-
-
--- simulator.h
+++
@@ -1,8 +0,0 @@
-class Simulator : public Robot {
- public:
-   explicit Simulator(int robot_size) : size(robot_size) {}
-
- private:	 
-   int x;
-   int y;
-   const int size;   
